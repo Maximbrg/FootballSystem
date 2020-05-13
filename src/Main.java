61936f3b7ebd925758cbd3d8ee1207f59ea83f32
Shachar Ron <shacharron101@gmail.com>
2020-05-13 14:24:43 +0300

    add dataAccess package

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 
+import DataAccess.DBConnector;
+import DataAccess.UserSQL;
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
+//        DBConnector.getConnection();
+//        UserSQL userSQL=UserSQL.getInstance();
+//        userSQL.getAll();
+        //userSQL.get(1);
+        //User user=new Referee("inbar", RefereeType.MAIN,8,"123","inbar1");
+        //userSQL.save(user);
+        //userSQL.delete(user);
     }
 }
